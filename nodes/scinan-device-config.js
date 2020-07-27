284a782acfe8baca689bd0c1ac2316e5d3a0e1ca
Lars Ødegaard <lars@kodeworks.no>
2020-07-27 22:25:01 +0200

    fix error when fetching devices on node configuration

--- nodes/scinan-device-config.js
+++ nodes/scinan-device-config.js
@@ -2,9 +2,14 @@ const scinan = require('../utils/api')
 const { getTimestamp, getStatus } = require('../utils/utils');
 const { AUTHORIZATION_URL, LIST_URL, USER_AGENT, CLIENT_ID, REDIRECT_URI } = require('../utils/constants');
 
+var node;
+
 module.exports = function(RED) {
     function ScinanDeviceConfigNode(config) {
         RED.nodes.createNode(this, config);
+
+        node = this;
+
     }
 
     RED.nodes.registerType("scinan-device-config",ScinanDeviceConfigNode,{
@@ -15,8 +20,10 @@ module.exports = function(RED) {
     });
 
     RED.httpAdmin.get("/scinan/devices", function(req, res) {
-        const server = RED.nodes.getNode(req.serverNodeId);
-        const api = new scinan({})
+        const api = new scinan({
+            username: node.credentials.username,
+            password: node.credentials.password
+        })
         var deviceList = []
         api.getDevices({}, function name(err, devices) {
             devices.forEach(function(device) {
